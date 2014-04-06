69ad7d536d4618e4c9a13d4e23b9abe05007bad0
ianfield@live.com <ianfield@live.com>
2014-04-05 17:43:49 -0700

    Update to stage

--- sites/all/modules/custom/features/produce/produce.js
+++ sites/all/modules/custom/features/produce/produce.js
@@ -10,16 +10,13 @@
 	  	function update() {
 			var allVals = new Array();
 	    	var values;
-			URL = "http://api.yummly.com/v1/api/recipes?_app_id=0471e0f5&_app_key=65fc767cfb1f236750b675bb1d022f9d&requirePictures=true&p=";
-			params = "";
+			URL = "http://api.yummly.com/v1/api/recipes?_app_id=0471e0f5&_app_key=65fc767cfb1f236750b675bb1d022f9d&requirePictures=true&";
 	     	$('.view-content :checked').each(function() {
 	       		allVals.push($(this).val());
 	     	});
 			for (var i = 0; i < allVals.length; i++){
-				URL += allVals[i] + "&";
+				URL += "allowedIngredient[]=" + allVals[i].toLowerCase() + "&";
 			}
-	     	values = allVals.join('\n');
-	     	console.log(values);
 	  	}
 	 	$(function() {
 	   		$('input').click(update);
@@ -27,9 +24,20 @@
 	 	});
 
 		$('#button').click(function() {
-			alert (URL);
 			document.cookie = "query = " + URL;
+			window.location ="/recipes";
 		});
+	$('.produce-holder').toggle(
+       function(event) {
+         $(this).find('input').attr('checked', true);
+         $(this).children("img").addClass( 'border' );
+       },
+       function(event) {
+         $(this).find('input').attr('checked', false);
+         $(this).children("img").removeClass( 'border' );
+       }
+     );
+
     }
   };
 }(jQuery));
