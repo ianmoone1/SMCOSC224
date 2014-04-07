3da380ec49a1bd28e04af410340bd5b2c53f414d
ianfield@live.com <ianfield@live.com>
2014-04-06 21:58:51 -0700

    Adding advanced search js and css tweaks

--- sites/all/modules/custom/features/produce/produce.js
+++ sites/all/modules/custom/features/produce/produce.js
@@ -20,44 +20,45 @@
 			}
 	  	}
 
-	  	function replaceString(){
+		function replaceString(){
 		
-			var dairy = "allowedAllergy[]=396^Dairy-Free"
-			var egg = "allowedAllergy[]=397^Egg-Free"
-			var gluten = "allowedAllergy[]=393^Gluten-Free"
-			var peanut = "allowedAllergy[]=394^Peanut-Free"
-			var seafood = "allowedAllergy[]=398^Seafood-Free"
-			var sesame = "allowedAllergy[]=399^Sesame-Free"
-			var soy = "allowedAllergy[]=400^Soy-Free"
-			var sulfite = "allowedAllergy[]=401^Sulfite-Free"
-			var treenut = "allowedAllergy[]=395^Tree Nut-Free"
-			var wheat = "allowedAllergy[]=392^Wheat-Free"
+			var dairy = "allowedAllergy[]=396^Dairy-Free";
+			var egg = "allowedAllergy[]=397^Egg-Free";
+			var gluten = "allowedAllergy[]=393^Gluten-Free";
+			var peanut = "allowedAllergy[]=394^Peanut-Free";
+			var seafood = "allowedAllergy[]=398^Seafood-Free";
+			var sesame = "allowedAllergy[]=399^Sesame-Free";
+			var soy = "allowedAllergy[]=400^Soy-Free";
+			var sulfite = "allowedAllergy[]=401^Sulfite-Free";
+			var treenut = "allowedAllergy[]=395^Tree Nut-Free";
+			var wheat = "allowedAllergy[]=392^Wheat-Free";
 			
 				var str = URL;
-				str = str.replace("Dairy Free",dairy);
-				str = str.replace("Egg Free",egg);
-				str = str.replace("Gluten Free",gluten);
-				str = str.replace("Peanut Free",peanut);
-				str = str.replace("Seafood Free",seafood);
-				str = str.replace("Sesame Free",sesame);
-				str = str.replace("Soy Free",soy);
-				str = str.replace("Sulfite Free",sulfite);
-				str = str.replace("Treenut Free",treenut);
-				str = str.replace("Wheat Free",wheat);
+				str = str.replace("allowedIngredient[]=dairy free",dairy);
+				str = str.replace("allowedIngredient[]=egg free",egg);
+				str = str.replace("allowedIngredient[]=gluten free",gluten);
+				str = str.replace("allowedIngredient[]=peanut free",peanut);
+				str = str.replace("allowedIngredient[]=seafood free",seafood);
+				str = str.replace("allowedIngredient[]=sesame free",sesame);
+				str = str.replace("allowedIngredient[]=soy free",soy);
+				str = str.replace("allowedIngredient[]=sulfite free",sulfite);
+				str = str.replace("allowedIngredient[]=treenut free",treenut);
+				str = str.replace("allowedIngredient[]=wheat free",wheat);
 				
-			var vegan = "allowedDiet[]=386^Vegan"
-			var lactoVeg = "allowedDiet[]=388^Lacto%20vegetarian"
-			var vegetarian = "allowedDiet[]=389^Ovo%20vegetarian"
-			var pescetarian = "allowedDiet[]=390^Pescetarian"
-			var ovoVeg = "allowedDiet[]=389^Ovo%20vegetarian"
+			var vegan = "allowedDiet[]=386^vegan";
+			var lactoVeg = "allowedDiet[]=388^Lacto%20vegetarian";
+			var vegetarian = "allowedDiet[]=389^Ovo%20vegetarian";
+			var pescetarian = "allowedDiet[]=390^Pescetarian";
+			var ovoVeg = "allowedDiet[]=389^Ovo%20vegetarian";
 			
-			str = str.replace("vegan",vegan);
-			str = str.replace("Vegetarian",vegetarian);
-			str = str.replace("Pescetarian",pescetarian);
-			str = str.replace("Ovo Vegetarian",ovoVeg);
-			str = str.replace("Lacto Vegetarian",lactoVeg);
+			str = str.replace("allowedIngredient[]=vegan",vegan);
+			str = str.replace("allowedIngredient[]=vegetarian",vegetarian);
+			str = str.replace("allowedIngredient[]=Pescetarian",pescetarian);
+			str = str.replace("allowedIngredient[]=Ovo Vegetarian",ovoVeg);
+			str = str.replace("allowedIngredient[]=Lacto Vegetarian",lactoVeg);
 			
 				URL=str;
+				alert(URL);
 		}
 	 	$(function() {
 	   		$('input').click(update);
